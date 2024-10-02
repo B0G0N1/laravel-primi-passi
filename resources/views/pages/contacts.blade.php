6c29f17aa7846297b55a7d56f3cfbc12260f8ab0
B0G0N1 <michelebogoni2001@gmail.com>
2024-10-02 05:39:38 +0200

    [UPD] Add comments, FINAL PROJECT

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Importa il CSS di Bootstrap da una CDN per lo styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -11,15 +12,20 @@
 </head>
 
 <body>
+    <!-- Intestazione della pagina con sfondo scuro -->
     <header class="bg-dark">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
+                    <!-- Menu di navigazione non ordinato (nessun bullet point) con link interni -->
                     <ul class="list-unstyled d-flex m-0 py-4">
+                        <!-- Link alla homepage, con stile del testo bianco e senza decorazione -->
                         <li class="px-3"><a class="text-decoration-none text-white"
                                 href="{{ route('homepage') }}">Homepage</a></li>
+                        <!-- Link alla pagina "About", stesso stile -->
                         <li class="px-3"><a class="text-decoration-none text-white"
                                 href="{{ route('about') }}">About</a></li>
+                        <!-- Link alla pagina "Contacts", stesso stile -->
                         <li class="px-3"><a class="text-decoration-none text-white"
                                 href="{{ route('contacts') }}">Contacts</a></li>
                     </ul>
@@ -27,10 +33,13 @@
             </div>
         </div>
     </header>
+
+    <!-- Contenuto principale della pagina -->
     <main>
         <div class="container">
             <div class="row">
                 <div class="col-12">
+                    <!-- Stampa il titolo e il messaggio passati dal controller -->
                     <h1>{{ $title }}</h1>
                     <h3>{{ $message }}</h3>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Definisce la route per la homepage, utilizzando una funzione anonima.
+// Viene passato il titolo e il messaggio alla vista 'home'.
 Route::get('/', function() {
-    $title = 'Homepage Laravel Primi Passi';
-    $message = 'Pagina principale';
+    $title = 'Homepage Laravel Primi Passi'; // Titolo della pagina
+    $message = 'Pagina principale'; // Messaggio della pagina
     
+    // Restituisce la vista 'home' con le variabili 'title' e 'message'
     return view('home', compact('title', 'message'));
-})->name('homepage');
+})->name('homepage'); // Nome della route
 
+
+// Definisce la route per la pagina "About Us".
 Route::get('/about', function() {
     $title = 'About Us';
     $message = 'Lorem ipsum dolor sit amet';
@@ -27,9 +32,12 @@ Route::get('/about', function() {
     return view('pages.about', compact('title', 'message'));
 })->name('about');
 
+
+// Definisce la route per la pagina dei contatti.
 Route::get('/contacts', function() {
     $title = 'Contatti';
     $message = 'Contattaci per ricevere le nostre ultime notizie.';
     
+    // Restituisce la vista 'pages.contacts' con le variabili 'title' e 'message'
     return view('pages.contacts', compact('title', 'message'));
 })->name('contacts');
